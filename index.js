c2cdccd22af43baa8b2a97906d8ab43b356047e2
Rohit <rohitarjunagi@gmail.com>
2016-07-06 17:49:23 +1000

    Use body-parser to parse data posted to the app

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+//use the json body parser to parse the request body
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -19,4 +21,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
