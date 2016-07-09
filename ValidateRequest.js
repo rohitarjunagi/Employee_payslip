ded9ff6b2521b247a01ba898d6d2b4cde61b28e1
Rohit <rohitarjunagi@gmail.com>
2016-07-09 18:47:27 +1000

    Implement a new function for validating date range

--- ValidateRequest.js
+++ ValidateRequest.js
@@ -120,4 +120,65 @@ function checkSuperRate(employee) {
     return false
   }
   return true;
+
+function checkDateRange(employee) {
+  //if payment start date does not exist or if payment start date is not of type string, return false
+  if (!employee.hasOwnProperty('payment_start_date') || typeof employee.payment_start_date !== 'string') {
+    return false;
+  }
+  // if payment start date does not contain - then return false
+  if (employee.payment_start_date.indexOf('-') == -1) {
+    return false;
+  }
+
+  var dateRange = employee.payment_start_date.split('-');
+  var startDate = new Date(dateRange[0]);
+  var endDate = new Date(dateRange[1]);
+  //if sart or end date is invalid, return false
+  if (startDate == 'Invalid Date' || endDate == 'Invalid Date') {
+    return false;
+  }
+  // get date difference in milliseconds
+  var dateDiff = Math.abs(endDate - startDate);
+  //check if date range is not a number. If yes, return false
+  if (isNaN(dateDiff)) {
+    return false;
+  }
+  // divide dateDiff by 1000 * 3600 * 24 to get difference in days
+  var daysDiff = Math.round((dateDiff / 86400000) + 1);
+  /* if month is jan, mar, may, july, aug, oct, dec and
+     difference in days is less than 31 oe greater than 31,
+     return false
+
+     if month is feb and difference in days is less than 28
+     or greater than 29, return false
+
+     if month is apr, jun, sep, nov and difference in days
+     is less than 30 or greater than 30, return false
+    */
+  switch (startDate.getMonth()) {
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+    case 7:
+    case 9:
+    case 11:
+      if (daysDiff !== 31) return false;
+      else return true;
+      break;
+    case 1:
+      if (daysDiff < 28 || daysDiff > 29) return false;
+      else return true;
+      break;
+    case 3:
+    case 5:
+    case 8:
+    case 10:
+      if (daysDiff !== 30) return false;
+      else return true;
+      break;
+    default:
+      return false;
+  }
 }
