036b69a2866ef4f7ab2594348f0d0dcfceaf5ff7
Rohit <rohitarjunagi@gmail.com>
2017-01-09 13:16:08 +1100

    Change the test cases

--- test/ProcessEmployeeData-test.js
+++ test/ProcessEmployeeData-test.js
@@ -7,49 +7,40 @@ var processEmployeePostData = new processEmployeeData();
 describe('Test the ProcessEmployeeData library', function() {
   describe('#processResponse()', function() {
     it('The response should have the right values for the correct input data#try 1', function(done) {
-      var content_type = "application/json";
       var request = {
-        "employee_data": [{
           "first_name": "David",
           "last_name": "Rudd",
-          "annual_salary": 23400,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }]
+          "annual_salary": "23400",
+          "super_rate": "10%"
       };
-      processEmployeePostData.processResponse(request, content_type, function(err, data) {
+      processEmployeePostData.processResponse(request, function(err, data) {
         if (err) return done(err);
-        assert.equal(data.response[0].name, "David Rudd");
-        assert.equal(data.response[0].pay_period, '01 March - 31 March');
-        assert.equal(data.response[0].gross_income, "1950");
-        assert.equal(data.response[0].income_tax, "82");
-        assert.equal(data.response[0].net_income, "1868");
-        assert.equal(data.response[0].super, "195");
+        assert.equal(data.name, "David Rudd");
+        assert.equal(data.gross_income, "1950");
+        assert.equal(data.income_tax, "82");
+        assert.equal(data.net_income, "1868");
+        assert.equal(data.super, "195");
+        assert.equal(data.pay, "1673");
 
         done();
       })
     })
 
     it('The response should have the right values for the correct input data#try 2', function(done) {
-      var content_type = "application/json";
       var request = {
-        "employee_data": [{
           "first_name": "Chris",
           "last_name": "Hadfield",
-          "annual_salary": 85000,
-          "super_rate": "20%",
-          "payment_start_date": "01 May - 31 May"
-        }]
+          "annual_salary": "85000",
+          "super_rate": "20%"
       };
-      processEmployeePostData.processResponse(request, content_type, function(err, data) {
+      processEmployeePostData.processResponse(request, function(err, data) {
         if (err) return done(err);
-        assert.equal(data.response[0].name, "Chris Hadfield");
-        assert.equal(data.response[0].pay_period, '01 May - 31 May');
-        assert.equal(data.response[0].gross_income, "7083");
-        assert.equal(data.response[0].income_tax, "1616");
-        assert.equal(data.response[0].net_income, "5467");
-        assert.equal(data.response[0].super, "1417");
-
+        assert.equal(data.name, "Chris Hadfield");
+        assert.equal(data.gross_income, "7083");
+        assert.equal(data.income_tax, "1616");
+        assert.equal(data.net_income, "5467");
+        assert.equal(data.super, "1417");
+        assert.equal(data.pay, "4050");
         done();
       })
     })
--- test/ValidateRequest-test.js
+++ test/ValidateRequest-test.js
@@ -4,113 +4,14 @@ var validateRequest = require('../ValidateRequest');
 var validateEmployees = new validateRequest();
 
 describe('Test the ValidateRequest library', function() {
-  describe('#validateRequestBody()', function() {
-    it('should validate the request body', function(done) {
-
-      var request;
-      var content_type = 'application/json'
-      var request = {
-        "employee_data": [{
-          "first_name": "David",
-          "last_name": "Rudd",
-          "annual_salary": 2340,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }, {
-          "first_name": "Chen",
-          "last_name": "Ryan",
-          "annual_salary": 300000,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }]
-      };
-      validateEmployees.validateRequestBody(request, content_type, function(err) {
-        if (err) return done(err);
-        done();
-      })
-    });
-    it('should produce an error for invalid request header', function(done) {
-      var content_type = 'application/xml'
-      var request = {
-        "employee_data": [{
-          "first_name": "David",
-          "last_name": "Rudd",
-          "annual_salary": 2340,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }, {
-          "first_name": "Chen",
-          "last_name": "Ryan",
-          "annual_salary": 300000,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }]
-      };
-      validateEmployees.validateRequestBody(request, content_type, function(err) {
-        assert.equal(err.message, 'Only Json content-types are processed for now!');
-        done();
-      })
-    });
-
-    it('should produce an error for empty body', function(done) {
-      var content_type = 'application/json';
-      var request = {};
-      validateEmployees.validateRequestBody(request, content_type, function(err) {
-        assert.equal(err.message, 'Request Body is empty!');
-        done();
-      });
-    });
-
-    it('should produce an error for no employee data', function(done) {
-      var content_type = 'application/json';
-      var request = {
-        "garbage_data": [{
-          "first_name": "David",
-          "last_name": "Rudd",
-          "annual_salary": 2340,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }, {
-          "first_name": "Chen",
-          "last_name": "Ryan",
-          "annual_salary": 300000,
-          "super_rate": "10%",
-          "payment_start_date": "01 March - 31 March"
-        }]
-      };
-      validateEmployees.validateRequestBody(request, content_type, function(err) {
-        assert.equal(err.message, 'Request Body does not contain the employee_data!');
-        done();
-      });
-    });
-
-    it('should produce an error for employee data being zero', function(done) {
-      var content_type = 'application/json';
-      var request = {
-        "employee_data": []
-      };
-      validateEmployees.validateRequestBody(request, content_type, function(err) {
-        assert.equal(err.message, 'Employee_data Length is 0!');
-        done();
-      });
-    });
-  });
-
   describe('#validateRequestData()', function() {
     it('should validate the correct request data', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": 2340,
-        "super_rate": "10%",
-        "payment_start_date": "01 March - 31 March"
-      }, {
-        "first_name": "Chen",
-        "last_name": "Ryan",
-        "annual_salary": 300000,
-        "super_rate": "10%",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "2340",
+        "super_rate": "10%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         console.log(err);
         if (err) return done(err);
@@ -119,16 +20,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if last name is not a type of string', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": 8370,
-        "annual_salary": 2340,
-        "super_rate": "10%",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "2340",
+        "super_rate": "10%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Last Name in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Last Name');
           return done();
         }
         done();
@@ -136,16 +36,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if last name does not have first character as a letter', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "1Rudd",
-        "annual_salary":2340,
-        "super_rate": "10%",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary":"2340",
+        "super_rate": "10%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Last Name in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Last Name');
           return done();
         }
         done();
@@ -153,16 +52,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if super is more than 50%', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": 23340,
-        "super_rate": "80%",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "23340",
+        "super_rate": "80%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Super Rate in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Super Rate');
           return done();
         }
         done();
@@ -170,16 +68,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if super rate has more than 1 occurence of %', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": 23340,
-        "super_rate": "asdfbmbnmbnmbnmbnm",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "23340",
+        "super_rate": "asdfbmbnmbnmbnmbnm"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Super Rate in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Super Rate');
           return done();
         }
         done();
@@ -187,16 +84,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if super rate does not have a %', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": 23340,
-        "super_rate": "80",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "23340",
+        "super_rate": "80"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Super Rate in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Super Rate');
           return done();
         }
         done();
@@ -204,16 +100,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if super rate has an alphabet %', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": 23340,
-        "super_rate": "abcde%%",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "23340",
+        "super_rate": "abcde%%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Super Rate in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Super Rate');
           return done();
         }
         done();
@@ -221,16 +116,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if annual salary is less than 0', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": -100,
-        "super_rate": "10%",
-        "payment_start_date": "01 March - 31 March"
-      }];
+        "annual_salary": "-100",
+        "super_rate": "10%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Annual Salary in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Annual Salary');
           return done();
         }
         done();
@@ -238,16 +132,15 @@ describe('Test the ValidateRequest library', function() {
     });
 
     it('should produce an error if payment start date is invalid', function(done) {
-      var request = [{
+      var request = {
         "first_name": "David",
         "last_name": "Rudd",
-        "annual_salary": 23000,
-        "super_rate": "10%",
-        "payment_start_date": "01 - 31 March"
-      }];
+        "annual_salary": "23000",
+        "super_rate": "10%"
+      };
       validateEmployees.validateRequestData(request, function(err) {
         if (err) {
-          assert.equal(err.message, 'Incorrect Date Range in Employee No: 0');
+          assert.equal(err.message, 'Incorrect Date Range');
           return done();
         }
         done();
